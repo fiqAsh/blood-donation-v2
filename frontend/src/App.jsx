ae74521ad674555971030e0b5df278935b4f1e9e
fiqAsh <ashfiq625@gmail.com>
2025-04-03 00:57:41 +0600

    initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-gray-100 text-gray-800 justify-center align-middle flex text-3xl font-bold">
+      App
+    </div>
+  );
+};
+
+export default App;
